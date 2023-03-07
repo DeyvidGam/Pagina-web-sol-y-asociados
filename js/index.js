adda37b0def5079ec2bc9bde16d930ed999c161a
Deyvid Gamboa <deyvidgame123@gmail.com>
2023-03-06 20:42:14 -0500

    Pagina web

---
+++ js/index.js
@@ -0,0 +1,64 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.navbar',{
+    duration: 1800,
+    origin: 'bottom', 
+    distance: '-100px',
+    reset: true,
+    viewFactor: 0.5,
+});
+
+sr.reveal('.img',{
+
+    duration: 1800 ,
+    origin: 'bottom', 
+    distance: '-200px',
+    reset: true,
+    viewFactor: 0.65
+});
+sr.reveal('.text',{
+
+    duration: 1800,
+    origin: 'bottom', 
+    distance: '-200px',
+    easing: 'ease-in-out',
+    reset: true,
+    viewFactor: 0.5,
+});
+
+sr.reveal('.somos',{
+
+    duration: 1500,
+    origin: 'left', 
+    distance: '-100px',
+    easing: 'ease-in-out',
+    reset: true,
+    viewFactor: 0.5,
+});
+sr.reveal('.servicios',{
+
+    duration: 1500,
+    origin: 'right', 
+    distance: '-100px',
+    easing: 'ease-in-out',
+    reset: true,
+    viewFactor: 0.5,
+});
+sr.reveal('.contactanos',{
+
+    duration: 1500,
+    origin: 'left', 
+    distance: '-100px',
+    easing: 'ease-in-out',
+    reset: true,
+    viewFactor: 0.7,
+});
+
+sr.reveal('.content',{
+
+    duration: 1500,
+    origin: 'buttom', 
+    distance: '100px',
+    
+
+});
